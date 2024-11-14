866bb48e72a0d3cc87e5fae6156a2b277c315464
FABIANC0505 <fabian.carmona.ttc.616@unilibre.edu.co>
2024-11-13 19:19:24 -0500

    ejercicio3

---
+++ taller3.py
@@ -0,0 +1,4 @@
+frase1 = input("Introduce la primera parte de la frase: ")
+frase2 = input("Introduce la segunda parte de la frase: ")
+fraseCompleta = frase1 + " " + frase2 
+print("\nLa frase completa es: " + fraseCompleta)
