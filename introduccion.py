8af1498607a9966a6b1c0f526ff32f9f4e9d5074
FABIANC0505 <fabian.carmona.ttc.616@unilibre.edu.co>
2024-11-13 19:04:02 -0500

    Taller 1

--- introduccion.py
+++ introduccion.py
@@ -1,1 +1,10 @@
+texto = input("Coloca un texto: ") 
+numeroEntero = int(input("Introduce un número entero: ")) 
+numeroDecimal = float(input("Escribe un numero decimal: "))  
+valorBooleano = input("Coloca un valor booleano: ").lower() == 'true'  
+
+print("\nValor de la cadena de texto:", texto)
+print("Valor del número entero:", numeroEntero)
+print("Valor del número decimal:", numeroDecimal)
+print("Valor booleano:", valorBooleano)
 
